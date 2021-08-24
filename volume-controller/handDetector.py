7bf488cdadbf3279ea531c9730e775cb4da15f5b
Nicolás Ketterer <nicolas.ketterer@cidla.org>
2021-08-23 23:42:49 -0300

    Volume Controller - Pynput

--- volume-controller/handDetector.py
+++ volume-controller/handDetector.py
@@ -1,20 +1,18 @@
 import mediapipe as mp
 import cv2
 
-#Initializations: static code
+# Initializations: static code
 mpHands = mp.solutions.hands
 mpDraw = mp.solutions.drawing_utils
 
 
-
 class HandDetector:
     def __init__(self, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5):
-        #when the mediapipe is first started, it detects the hands. After that it tries to track the hands
-        #as detecting is more time consuming than tracking. If the tracking confidence goes down than the
-        #specified value then again it switches back to detection
+        # when the mediapipe is first started, it detects the hands. After that it tries to track the hands
+        # as detecting is more time consuming than tracking. If the tracking confidence goes down than the
+        # specified value then again it switches back to detection
         self.hands = mpHands.Hands(max_num_hands=max_num_hands, min_detection_confidence=min_detection_confidence,
                                    min_tracking_confidence=min_tracking_confidence)
-
 
     def findHandLandMarks(self, image, handNumber=0, draw=False):
         originalImage = image
@@ -23,7 +21,8 @@ class HandDetector:
         landMarkList = []
 
         if results.multi_hand_landmarks:  # returns None if hand is not found
-            hand = results.multi_hand_landmarks[handNumber] #results.multi_hand_landmarks returns landMarks for all the hands
+            # results.multi_hand_landmarks returns landMarks for all the hands
+            hand = results.multi_hand_landmarks[handNumber]
 
             for id, landMark in enumerate(hand.landmark):
                 # landMark holds x,y,z ratios of single landmark
@@ -32,6 +31,7 @@ class HandDetector:
                 landMarkList.append([id, xPos, yPos])
 
             if draw:
-                mpDraw.draw_landmarks(originalImage, hand, mpHands.HAND_CONNECTIONS)
+                mpDraw.draw_landmarks(
+                    originalImage, hand, mpHands.HAND_CONNECTIONS)
 
         return landMarkList
--- volume-controller/volumeController.py
+++ volume-controller/volumeController.py
@@ -2,42 +2,64 @@ from handDetector import HandDetector
 import cv2
 import math
 import numpy as np
+import time
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from pynput.keyboard import Key, Controller
 
 handDetector = HandDetector(min_detection_confidence=0.7)
 webcamFeed = cv2.VideoCapture(0)
 
-#Volume related initializations
+# Volume related initializations
 devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
-IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
-#print(volume.GetVolumeRange()) --> (-65.25, 0.0)
+# print(volume.GetVolumeRange()) --> (-65.25, 0.0)
 
 while True:
     status, image = webcamFeed.read()
     handLandmarks = handDetector.findHandLandMarks(image=image, draw=True)
 
     if(len(handLandmarks) != 0):
-        #for volume control we need 4th and 8th landmark
-        #details: https://google.github.io/mediapipe/solutions/hands
+        # for volume control we need 4th and 8th landmark
+        # details: https://google.github.io/mediapipe/solutions/hands
+        # 4 Thumb
+        # 8 Index
         x1, y1 = handLandmarks[4][1], handLandmarks[4][2]
         x2, y2 = handLandmarks[8][1], handLandmarks[8][2]
-        length = math.hypot(x2-x1, y2-y1)
-        print(length)
+        length_4_8 = math.hypot(x2-x1, y2-y1)
+        # print(length_4_8)
 
-        #Hand range(length): 50-250
-        #Volume Range: (-65.25, 0.0)
+        # Hand range(length_4_8): 50-250
+        # Volume Range: (-65.25, 0.0)
 
-        volumeValue = np.interp(length, [50, 250], [-65.25, 0.0]) #coverting length to proportionate to volume range
+        # coverting length_4_8 to proportionate to volume range
+        volumeValue = np.interp(length_4_8, [50, 250], [-65.25, 0.0])
         volume.SetMasterVolumeLevel(volumeValue, None)
-
 
         cv2.circle(image, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
         cv2.circle(image, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
         cv2.line(image, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        
+
+        # Media Keys
+        # Next Track
+        # 12 Middle
+
+        keyboard = Controller()
+
+        x3, y3 = handLandmarks[12][1], handLandmarks[12][2]
+        length_8_12 = math.hypot(x3-x2, y3-y2)
+        # print(length_8_12)
+        if(length_8_12 < 50):
+            # print("HELLO WORLD")
+            keyboard.press(Key.media_next)
+            time.sleep(1) #more work
+
+        cv2.circle(image, (x3, y3), 15, (255, 0, 255), cv2.FILLED)
+        cv2.line(image, (x2, y2), (x3, y3), (255, 255, 0 ), 3)
 
     cv2.imshow("Volume", image)
     cv2.waitKey(1)
